a9c635ecc93a515042f5e593ed64fb5ddbb24a45
Brandon Medina <brandonmedina@MacBook-Air-de-Brandon.local>
2026-06-16 23:54:50 -0600

    fix: add cors

--- src/app.js
+++ src/app.js
@@ -7,7 +7,11 @@ const pronosticoRoutes = require("./presentation/routes/pronosticoRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
+  }),
+);
 
 app.use(express.json());
 
